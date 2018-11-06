dd19628f63b6641c52c42d80ac2f5eb8e309188c
gabriel schiavo <gabriel@lewagon.org>
2018-11-06 15:34:12 -0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :users, only: [:new, :create, :edit, :update] do
-    resources :products
+  resources :users, only: [:new, :create, :edit, :update] do 	#Rotas edit & update ñ serão utilizadas na apresentação
+    resources :products # Rotas destroy, edit & update ñ serão utilizadas na apresentação 
+    resources :transactions, :only: [:create, :index]
   end
 end
