013af33f582c649da73970e5cc91948f71190039
gabriel schiavo <gabriel@lewagon.org>
2018-11-06 11:29:49 -0200

    Init seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+puts 'Creating cocktails...'
+cocktails_attributes = [
+  {
+    name:         'Spicy Grand Margarita'
+  },
+  {
+    name:         'Marguierita'
+  },
+  {
+    name:         'Drinks'
+  },
+  {
+    name:         'Gym Martinho'
+  },
+  {
+    name:         'Dry Martinho'
+  },
+  {
+    name:         'Sake'
+  }
+]
+Cocktail.create!(cocktails_attributes)
+puts 'Finished!'
+
+puts 'Creating ingredients...'
+20.times do 
+	Ingredient.create(name: Faker::Food.fruits)
+end
+puts 'Finished!'
+puts 'Creating doses...'
+# i = 0
+10.times do
+	Dose.create(
+		ingredient_id: Faker::Number.between(1, 10),
+		cocktail_id: 	Faker::Number.between(1, 10),
+		description: 	Faker::RickAndMorty.quote,
+		nacionality: 	Faker::Address.country
+	)
+end 
+puts  'Finished' 
