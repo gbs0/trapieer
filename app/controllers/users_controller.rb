f0c9ec446a1f4327f4611b4c31e29b5cb6872f5f
Airton Ponce <airtonponce@outlook.com>
2018-11-06 19:08:24 -0200

    implemented user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    user = User.new(user_params)
+    user.save
+    redirect_to root_path
   end
 
-  def edit
+  # acões edit e update não serão apresentadas
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  private
+
+  def user_params
+    params.require(:user).permit( :name, :address, :email, :password, :balance, :avatar )
   end
 
-  def update
+  def set_user
+    @user = User.find(params[:id])
   end
+
 end
