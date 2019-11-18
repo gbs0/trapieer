72c1a5bf856c1d318f2039b4f9dad6def2708fe6
gabriel schiavo <gabriel@lewagon.org>
2019-11-18 19:12:11 +0000

    add more elements on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,42 +28,56 @@ puts 'creating users'
     balance: 50 ,
     avatar: Faker::Avatar.image("my-own-slug", "50x50")
   )
+
+
 puts 'creating products'
 
   malte = Product.create(
-    name: 'malte de qualidade',
+    name: 'Château Pilsen',
     category: 'malte',
-    origin:   'thailandia',
+    origin:   'Belgica 🇧🇪',
     quantity: 10,
-    price: 10,
+    price: 12.5,
     description:  'Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Per aumento de cachacis, eu reclamis. Diuretics paradis num copo é motivis de denguis.',
     user_id: User.find_by_name('admar').id
   )
-  malte.remote_image_url = 'https://res.cloudinary.com/aponce/image/upload/v1541701443/aidt5yn9dbzh3vdb0fve.jpg'
+  malte.remote_image_url = 'https://valbier.com.br/image/cache/catalog/produtos/Malte_chateau_Pilsen_162-750x750.jpg'
   malte.save
 
-    levedo = Product.create(
-    name: 'levedo de qualidade',
-    category: 'levedura',
-    origin:   'brasil',
-    quantity: 11,
-    price: 15,
+  malte0 = Product.create(
+    name: 'Château Peated (Defumado)',
+    category: 'malte',
+    origin:   'Belgica 🇧🇪',
+    quantity: 10,
+    price: 14.69,
     description:  'Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Per aumento de cachacis, eu reclamis. Diuretics paradis num copo é motivis de denguis.',
-    user_id: User.find_by_name('breno').id
+    user_id: User.find_by_name('admar').id
   )
-  levedo.remote_image_url = 'https://res.cloudinary.com/aponce/image/upload/v1541692487/ngfqmiujsrwlhlmvu66l.jpg'
-  levedo.save
+  malte0.remote_image_url = 'http://www.brejarte.com.br/loja/image/cache/catalog/malte-chateau-special-b-castle-malting-100g-ccc-220x220.jpg'
+  malte0.save
 
-    lupulo = Product.create(
-    name: 'lupulo de qualidade',
-    category: 'lupulo',
-    origin:   'japão',
-    quantity: 12,
-    price: 20,
-    description:  'Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Per aumento de cachacis, eu reclamis. Diuretics paradis num copo é motivis de denguis.',
-    user_id: User.find_by_name('cleiton').id
-  )
-  lupulo.remote_image_url = 'https://res.cloudinary.com/aponce/image/upload/v1541709120/ngxsjud3iqvj9nfpkokp.jpg'
-  lupulo.save
+  # levedo = Product.create(
+  #   name: 'levedo de qualidade',
+  #   category: 'levedura',
+  #   origin:   'brasil',
+  #   quantity: 11,
+  #   price: 15,
+  #   description:  'Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Per aumento de cachacis, eu reclamis. Diuretics paradis num copo é motivis de denguis.',
+  #   user_id: User.find_by_name('breno').id
+  # )
+  # levedo.remote_image_url = 'https://res.cloudinary.com/aponce/image/upload/v1541692487/ngfqmiujsrwlhlmvu66l.jpg'
+  # levedo.save
+
+  #   lupulo = Product.create(
+  #   name: 'lupulo de qualidade',
+  #   category: 'lupulo',
+  #   origin:   'Japão 🇯🇵',
+  #   quantity: 12,
+  #   price: 20,
+  #   description:  'Mussum Ipsum, cacilds vidis litro abertis. Leite de capivaris, leite de mula manquis sem cabeça. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Per aumento de cachacis, eu reclamis. Diuretics paradis num copo é motivis de denguis.',
+  #   user_id: User.find_by_name('cleiton').id
+  # )
+  # lupulo.remote_image_url = 'https://res.cloudinary.com/aponce/image/upload/v1541709120/ngxsjud3iqvj9nfpkokp.jpg'
+  # lupulo.save
 
   puts "seeds created"
